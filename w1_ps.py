e7f1dedba84d33f90a285572733f111be8b2ee11
Jaroslaw Glod <emmart@poczta.onet.pl>
2023-11-02 14:48:28 +0000

    lectures

---
+++ w1_ps.py
@@ -0,0 +1,28 @@
+print(6 + 7 * 2)
+print((2 + 3) * 2)
+print("Hello")
+print("Mister")
+
+var1 = 5
+var2 = 6
+print(5 + 6 - 5 / 6 + 5 * 6)
+print(var1 + var2 - var1 / var2 + var1 * var2)
+
+print(type(var1))
+print(type(var2))
+
+age = int(input("What is your age? "))
+print(f"You are {age} old and still alive???")
+
+# print(f"Next year you will be {age+1} years old")
+# print("Next year you will be", age+1)
+# print("Next year you will be " + str(age + 1))
+
+# cveonrt = casting
+
+name = input("What is your name? ")
+salary = float(input("What is your salary? "))
+# salary=round(salary,2)
+
+print("Your salary is now", format(salary * 0.9, ".2f"),
+      "I took a bite, hahahaha!!!!")
